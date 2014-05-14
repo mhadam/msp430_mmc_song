5f016704c1f2c2f9641b2e018a9a2c9d283459f4
oatsey <mikehadam@gmail.com>
2014-05-14 11:19:08 -0400

    stereo music

--- main.c
+++ main.c
@@ -3,11 +3,23 @@
 #include "MMC.h"
 #include "hal_SPI.h"
 #include "hal_MMC_hardware_board.h"
-#include "fifo.h"
+
+#define MAX_BUF 40
+
+struct RingBuffer {
+	//Where to add data
+	unsigned int head;
+	//Where to read data from
+	unsigned int tail;
+	unsigned char buffer[MAX_BUF];
+	//Need to distinguish between empty and full
+	unsigned char empty;
+};
 
 //Red onboard LED
 const unsigned char LED = BIT0;
 
+volatile struct RingBuffer txBuffer;
 volatile unsigned long currentBlock = 0; // current block being read from mmc device
 volatile unsigned int frameCount = 0;
 volatile unsigned int count = 0; // size of ring buffer
@@ -15,20 +27,52 @@ volatile unsigned int bufferReady = 0; // ready to receive samples
 volatile unsigned long size = 0;
 
 volatile unsigned char result = 0;
-volatile unsigned char channel = 0; // mono channel sample
+volatile unsigned char lchannel = 0; // left channel sample
+volatile unsigned char rchannel = 0; // right channel
 
 volatile unsigned long offset = 0; // size of the music sample data in bytes
 volatile unsigned long fileCount = 0; // number of samples read (left and right channels)
 
 volatile unsigned char header[4] = {0}; // used to store header information (6: 32-bit fields)
-volatile unsigned char load[1] = {0}; // used to store each byte spi read
+volatile unsigned char load[8] = {0}; // used to store each byte spi read
+
+void pushBuffer(unsigned char value) {
+	// Cannot push if the buffer is full
+	if (!txBuffer.empty && txBuffer.head == txBuffer.tail) { return; }
+	//Push back the length
+	txBuffer.buffer[txBuffer.head] = value;
+	//Reset head if it increments to MAX_BUF (size of the buffer)
+	if (MAX_BUF == ++txBuffer.head) {
+		txBuffer.head = 0;
+	}
+	//Just pushed a value, cannot be empty
+	txBuffer.empty = 0;
+}
+
+unsigned char popBuffer() {
+	//This is an error, trying to over-read the buffer
+	if (txBuffer.empty) {
+		return 0;
+	}
+	unsigned char value = txBuffer.buffer[txBuffer.tail];
+	if (MAX_BUF == ++txBuffer.tail) {
+		txBuffer.tail = 0;
+	}
+
+	//See if we just emptied the buffer
+	if (txBuffer.head == txBuffer.tail) {
+		txBuffer.empty = 1;
+	}
+	return value;
+}
 
 void main(void) {
 
 	WDTCTL = WDTPW + WDTHOLD;        // Stop watchdog timer
 
-	DCOCTL = CALDCO_16MHZ;
 	BCSCTL1 = CALBC1_16MHZ;
+	DCOCTL = CALDCO_16MHZ;
+	//BCSCTL3 |= XCAP_3; // set ext crystal osc.
 
 	//Reset SD card by cycling power
 	//Credit to Boris Cherkasskiy and his blog post on Launchpad+MMC
@@ -72,65 +116,88 @@ void main(void) {
 	}
 
 	// set timers and interrupts now that the header is read
-	TA0CCR0 = 255; // volume of sample (8 bit)
-	TA0CCR1 = 0;
-	TA0CTL = TASSEL_2 + MC_1 + ID_1; // Timer A0 with SMCLK, count UP, div by 2
+	TA1CCR0 = 255; // volume of sample (8 bit)
+	TA1CCR1 = 1; // important so that code in TACCR1 executes
+	TA1CCR2 = 1;
+	TA1CTL = TASSEL_2 + MC_1 + ID_1; // Timer A0 with SMCLK, count UP, div by 2
 
-	TA0CCTL0 = CCIE; // enable TACCR0 interrupt
-	TA0CCTL1 = OUTMOD_7; // enable TACCR1 interrupt w/ toggle/reset
+	TA1CCTL0 = CCIE; // enable TACCR0 interrupt
+	TA1CCTL1 = OUTMOD_7 + CCIE; // enable TACCR1 interrupt w/ toggle/reset
+	TA1CCTL2 = OUTMOD_7 + CCIE;
 
-	P1DIR |= BIT6; P1SEL |= BIT6; // set sound output pin
+	P2DIR |= BIT4 + BIT1; P2SEL |= BIT4 + BIT1; // set sound output pin
 
-	while (fifo_in_ready()) { // fill the buffer
-		spiReadFrame((unsigned char *)load, 1);
-		fifo_in(load[0]);
-		count++;
-		fileCount++;
-		frameCount++;
+	for (i = 0; i < 32; i++) { // fill the buffer
+		spiReadFrame((void *)load, 1);
+		pushBuffer(load[0]);
 	}
+
+	count = 32; // buffer is full now
+	frameCount = 9; // 3 (header) + 4 (fill buffer)
+	fileCount = 32;
 
 	_BIS_SR(GIE); // enable interrupts and sleep
 
-
 	while(1){
-		if (fifo_in_ready()) { // make sure the buffer isn't overloaded
-			spiReadFrame((unsigned char *)load, 1);
-			fifo_in(load[0] + 128);
+		if (count < 32) { // make sure the buffer isn't overloaded
+			spiReadFrame((void *)load, 8);
+			for (i = 0; i < 8; i++) {
+				pushBuffer(load[i] + 128);
+			}
 
-			//count++; // account for 8 samples added to buffer
-			fileCount++; // account for 8 samples towards end of data
+			count += 8; // account for 8 samples added to buffer
+			fileCount += 8; // account for 8 samples towards end of data
 			frameCount++; // an 8 byte frame has been read
 
-			if (frameCount == 512) {
-				currentBlock += 1; // move onto next block since all samples have been read
+			if (frameCount == 64) {
+				currentBlock += 512; // move onto next block since all samples have been read
 				mmcUnmountBlock(); // unmount old block
 
-				if (fileCount == offset) { // check for end of file
+				if (fileCount >= offset) { // check for end of file
 					currentBlock = 0;
 					fileCount = 0;
 				}
 
-				//CS_HIGH(); // maybe this will help?
-				//spiSendByte(0xFF);
+				CS_HIGH(); // this apparently speeds up the unmount/mount transition
+				spiSendByte(0xFF);
 
-				result = mmcMountBlock((long)(currentBlock*512), 512); // mount next block
+				result = mmcMountBlock(currentBlock, 512); // mount next block
 				frameCount = 0; // reset frame count
 			}
 		}else{
-			//LPM0; // sleep since the buffer is full
+			LPM0; // sleep since the buffer is full
 		}
 	}
 }
 
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer0_A0 (void) {
-	if (fifo_out_ready()){
-		channel = fifo_out(); // take a sample from the ring buffer
-		TACCR1 = channel;
-		//__bic_SR_register_on_exit(CPUOFF);
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void Timer1_A0 (void) {
+	if (count > 1) {
+		lchannel = popBuffer(); // take a sample from the ring buffer
+		rchannel = popBuffer(); // take a sample from the ring buffer
+		count = count - 2; // buffer is one smaller
 	}
+	LPM0_EXIT; // wake processor
 }
 
-#pragma vector=TIMER0_A1_VECTOR
-__interrupt void Timer0_A1 (void) {
+#pragma vector=TIMER1_A1_VECTOR
+__interrupt void Timer1_A1 (void) {
+	switch (TA1IV){ // interrupt flag reset by reading TAIV
+	case 0:
+		break;
+	case 2: // TACCR1
+		if (lchannel == 0) {
+			lchannel = 1;
+		}
+		TA1CCR1 = lchannel; // set left channel sample
+		break;
+	case 4: // TACCR2
+		if (rchannel == 0) {
+			rchannel == 1;
+		}
+		TA1CCR2 = rchannel; // set right channel sample
+		break;
+	default:
+		break;
+	}
 }
